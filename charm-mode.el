d3d642a633c93ea35e58cf9f84887df95fd87996
T L <tslimkemann42@gmail.com>
2018-04-04 20:17:59 +0000

    more functions

--- charm-mode.el
+++ charm-mode.el
@@ -9,6 +9,16 @@
   (setq font-lock-defaults '(charm-keywords))
   (setq font-lock-keywords-only t))
 
+(defun eval-charm-line ()
+  "Interpret a line of Charm"
+  (interactive)
+  (let ((charm-line (thing-at-point 'line t)))
+    (message charm-line)))
+
+(defun charm-repl ()
+  "Open a Charm REPL in a new buffer"
+  (interactive)
+  (start-process "charm-repl-process" "Charm REPL" "charm"))
 
 (add-to-list 'auto-mode-alist '("\\.charm\\'" . charm-mode))
 (provide 'charm-mode)
