f918f109de0f2b1205554958d798cd5ac96dca7c
T L <tslimkemann42@gmail.com>
2018-04-08 01:28:29 -0700

    correct buffer switching, charm-eval-line

--- charm-mode.el
+++ charm-mode.el
@@ -3,6 +3,9 @@
     ("-?[0-9]*\\.?[0-9]+" (0 font-lock-constant-face))
     ("\".+\"[[:blank:]]+pop" (0 font-lock-comment-face))
     ("[\^\\|[:space:]]\".+\"[\$\\|[:space:]]" (0 font-lock-string-face))))
+(defconst charm-buffer "Charm REPL")
+(defconst charm-path "/usr/bin/charm")
+
 
 (defun charm-enable-smm ()
   "Enable the Charm Syntax Minor Mode"
@@ -22,19 +25,30 @@
   "Major mode for writing Charm code."
   (charm-syntax-minor-mode))
 
-(defun eval-charm-line ()
-  "Interpret a line of Charm"
+(defun charm-eval-line ()
+  "Interpret a line of Charm in the Charm REPL."
   (interactive)
   (let ((charm-line (thing-at-point 'line t)))
+                                        ; make sure that we make the buffer in a new frame if it's not already there -- to avoid replacing our code with the repl
+    (when (null (get-buffer charm-buffer))
+      (switch-to-buffer-other-window charm-buffer))
+    (charm-repl)
+    (insert charm-line)
+    (comint-send-input)
     (message charm-line)))
 
+(defun charm-repl-here ()
+  "Open a Charm REPL in the Charm REPL buffer."
+  (make-comint-in-buffer "charm-repl-process" charm-buffer charm-path))
 
-(defvar charm-path "/usr/bin/charm")
 (defun charm-repl ()
-  "Open a Charm REPL in a new buffer"
+  "Open a Charm REPL in a new or existing buffer."
   (interactive)
-  (switch-to-buffer-other-window "Charm REPL")
-  (make-comint-in-buffer "charm-repl-process" "Charm REPL" charm-path)
+  (setq comint-prompt-read-only t)
+  (if (get-buffer charm-buffer)
+      (pop-to-buffer charm-buffer)
+    (switch-to-buffer charm-buffer))
+  (charm-repl-here)
   (charm-syntax-minor-mode))
 
 (add-to-list 'auto-mode-alist '("\\.charm\\'" . charm-mode))
